415eef506f7cdd486ab36d98cedbf4eadba1a759
Agustin <agustinxmtz@gmail.com>
2021-03-24 17:24:52 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,45 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+const sess= {
+    // secret: 'Super secret secret',
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore ({
+        db: sequelize
+    })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/'));
 
 // turn on routes
 app.use(routes);
 
-<<<<<<< HEAD
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+
+
