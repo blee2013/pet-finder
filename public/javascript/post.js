b4e2ce238edc2db6c465b7ff5bf171c901335219
MWPX777 <mwpx777@gmail.com>
2021-03-25 17:36:30 -0500

    working on multer implementation

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,29 +1,78 @@
-const handleImageUpload = event =>{
-    const files = event.target.files
-    const formData = new FormData()
-    formData.append('myFile', files[0])
+// const handleImageUpload = event =>{
+//     const files = event.target.files
+//     const formData = new FormData()
+//     formData.append('myFile', files[0])
 
-    if(!myImage.type.match(imageType)){
-        alert('Sorry, only images are allowed')
-        return;
-    }
-    if(myImage.size > (100*1024)){
-        alert('Sorry, the max image size is 100KB')
-        return;
-    }
+//     if(!myImage.type.match(imageType)){
+//         alert('Sorry, only images are allowed')
+//         return;
+//     }
+//     if(myImage.size > (100*1024)){
+//         alert('Sorry, the max image size is 100KB')
+//         return;
+//     }
 
-    fetch('/saveImage', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.path)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
+//     fetch('/saveImage', {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.path)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+// }
+
+
+// let storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       // uploads is the upload_folder_name
+//       cb(null, file.fieldname + ".png")
+//     }
+//   })
+  
+//   // define size of image for uploading
+//   const maxSize = 1 * 1000 * 1000;
+  
+//   let upload = multer({
+//     storage: storage,
+//     limits: {filesize: maxSize},
+//     fileFilter: function(req, file, cb) {
+  
+//       // set filetypes
+//       let filetypes = /jpg|jpeg|png/;
+//       let mimetype = filetypes.test(file.mimetype);
+  
+//       let extname = filetypes.test(path.extname(
+//         file.originalname).toLowerCase());
+  
+//         if(mimetype && extname) {
+//           return cb(null, true);
+//         }
+//         cb("Error: File upload only supports the " + "following filetypes - " + filetypes)
+  
+//     }
+//   })
+//   // mypic is name of file attribute
+//   .single("mypic");
+  
+//   app.get("/", function (req, res){
+//     res.render("post");
+//   })
+  
+//   app.post("/uploadProfilePicture", function (req, res, next){
+  
+//     // error middleware for multer file upload error
+//     upload(req, res, function(err){
+//       if (err){
+//         res.send(err)
+//       } else {
+//         res.send("Success, image uploaded!")
+//       }
+//     })
+//   })
 
 async function newFormHandler (event){
     console.log('post button clicked')
@@ -60,5 +109,3 @@ async function newFormHandler (event){
 
 document.querySelector('.petSearch-form').addEventListener('submit', newFormHandler);
 
-document.querySelector('#fileUpload').addEventListener('change', event =>{
-    handleImageUpload(event)})
--- server.js
+++ server.js
@@ -1,56 +1,5 @@
-
-// require('dotenv').config();
-// //const routes = require('./routes');
-// // import sequelize connection
-// const sequelize = require('./config/connection');
-// const db = require("./models");
-// const path = require('path');
-// const express = require('express');
-// const exphbs = require('express-handlebars');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(express.static('public'));
-
-// //app.use(routes);
-
-// // sync sequelize models to the database, then turn on the server
-// // turn on connection to db and server
-
-
-
-// const session = require('express-session');
-
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({ helpers });
-
-
-// const sess= {
-//     // secret: 'Super secret secret',
-//     secret: process.env.SECRET,
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore ({
-//         db: sequelize
-//     })
-// };
-
-// app.use(session(sess));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening'));
-// });
-
 require('dotenv').config();
+const multer = require('multer');
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -68,6 +17,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const helpers = require('./utils/helpers');
+const { addHook } = require('./models/Pet');
 const hbs = exphbs.create({ helpers });
 
 
@@ -84,16 +34,77 @@ const sess= {
 
 // for file uploading image
 app.use(fileupload())
+// view setup engine for Multer
+// app.set("ejsfolder", path.join(__dirname, "ejsfolder"))
+app.set('view engine', 'ejs')
+
+
 app.use(session(sess));
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set("ejsfolder", path.join(__dirname, "ejsfolder"))
+
+
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+
+      // Uploads is the Upload_folder_name
+      cb(null, "uploads")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now()+".jpg")
+  }
+})
+
+// define size of image for uploading
+const maxSize = 1 * 1000 * 1000;
+
+let upload = multer({
+  storage: storage,
+  limits: {filesize: maxSize},
+  fileFilter: function(req, file, cb) {
+
+    // set filetypes
+    let filetypes = /jpg|jpeg|png/;
+    let mimetype = filetypes.test(file.mimetype);
+
+    let extname = filetypes.test(path.extname(
+      file.originalname).toLowerCase());
+
+      if(mimetype && extname) {
+        return cb(null, true);
+      }
+      cb("Error: File upload only supports the " + "following filetypes - " + filetypes)
+
+  }
+})
+// mypic is name of file attribute
+.single("mypic");
+
+app.get("/", function (req, res){
+  res.render("homepage");
+})
+
+app.post("/uploadProfilePicture", function (req, res, next){
+
+  // error middleware for multer file upload error
+  upload(req, res, function(err){
+    if (err){
+      res.send(err)
+    } else {
+      res.send("Success, image uploaded!")
+    }
+  })
+})
 
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false}).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-});
+})
