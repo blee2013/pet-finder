53d2d97b513a9d59aa353c60167b7244e5dcfda5
Anthonyz28 <anthony.z28@hotmail.com>
2021-03-26 08:00:18 -0500

    post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,28 +1,262 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Post } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const sequelize = require('../config/connection');
+const { Pet } = require('../models');
+const withAuth = require('../utils/auth');
 
-
-
-router.post('/', (req, res) => {
-  console.log('postroute');
+router.post('/create', withAuth, (req, res) => {
+    console.log(req.body);
+    
+    Pet.create(
+      {
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description,
+        breed: req.body.breed,
+        age: req.body.age,
+        gender: req.body.gender,
+        type: req.body.type
+      },
+      {
+        where: {
+          id: req.body.id
+        }
+      },
+    
+    )
+      .then(dbPetData => {
+        if (!dbPetData) {
+          res.status(404).json({ message: 'No Pet found with this id' });
+          return;
+        }
+        res.json(dbPetData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+router.put('/update', withAuth, (req, res) => {
   console.log(req.body);
-  return
-//   Post.create({
-//     name: req.body.name,
-//     description: req.body.description,
-//     breed: req.body.breed,
-//     age: req.body.age,
-//     gender: req.body.gender,
-//     picture: req.body.picture,
-
-//   })
-//     .then(dbPostData => res.json(dbPostData))
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
+  
+  Pet.update(
+    {
+      id: req.body.id,
+      name: req.body.name,
+      image: req.body.image,
+      description: req.body.description,
+      breed: req.body.breed,
+      age: req.body.age,
+      gender: req.body.gender,
+      type: req.body.type
+    },
+    {
+      where: {
+        id: req.body.id
+      }
+    },
+  
+  )
+    .then(dbPetData => {
+      if (!dbPetData) {
+        res.status(404).json({ message: 'No Pet found with this id' });
+        return;
+      }
+      res.json(dbPetData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+router.delete('/delete/:id', withAuth, (req, res) => {
+
+ 
+   
+  Pet.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPetData => {
+      if (!dbPetData) {
+        res.status(404).json({ message: 'No Pet found with this id' });
+        return;
+      }
+      res.json(dbPetData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/',  (req, res) => {
+  // console.log(req.session);
+  Pet.findAll({
+    attributes : [
+      "id",
+      "name",
+      "image",
+      "description",
+      "breed",
+      "age",
+      "gender",
+      "type"
+    ]
+})
+.then(dbPetData => res.json(dbPetData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Pet.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      "id",
+      "name",
+      "image",
+      "description",
+      "breed",
+      "age",
+      "gender",
+      "type"
+    ]
+  })
+    .then(dbPetData => {
+      if (!dbPetData) {
+        res.status(404).json({ message: 'No Pet found with this id' });
+        return;
+      }
+      res.json(dbPetData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:breed', (req, res) => {
+  Pet.findOne({
+    where: {
+      id: req.params.breed
+    },
+    attributes: [
+      "id",
+      "name",
+      "image",
+      "description",
+      "breed",
+      "age",
+      "gender",
+      "type"
+    ]
+  })
+    .then(dbPetData => {
+      if (!dbPetData) {
+        res.status(404).json({ message: 'No Pet found with this breed' });
+        return;
+      }
+      res.json(dbPetData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:age', (req, res) => {
+  Pet.findOne({
+    where: {
+      id: req.params.age
+    },
+    attributes: [
+      "id",
+      "name",
+      "image",
+      "description",
+      "breed",
+      "age",
+      "gender",
+      "type"
+    ]
+  })
+    .then(dbPetData => {
+      if (!dbPetData) {
+        res.status(404).json({ message: 'No Pet found with this age' });
+        return;
+      }
+      res.json(dbPetData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:gender', (req, res) => {
+  Pet.findOne({
+    where: {
+      id: req.params.gender
+    },
+    attributes: [
+      "id",
+      "name",
+      "image",
+      "description",
+      "breed",
+      "age",
+      "gender",
+      "type"
+    ]
+  })
+    .then(dbPetData => {
+      if (!dbPetData) {
+        res.status(404).json({ message: 'No Pet found with this gender' });
+        return;
+      }
+      res.json(dbPetData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+// find one by type
+router.get('/:type', (req, res) => {
+    Pet.findOne({
+      where: {
+        id: req.params.type
+      },
+      attributes: [
+        "id",
+        "name",
+        "image",
+        "description",
+        "breed",
+        "age",
+        "gender",
+        "type"
+      ]
+    })
+      .then(dbPetData => {
+        if (!dbPetData) {
+          res.status(404).json({ message: 'No Pet found with this type' });
+          return;
+        }
+        res.json(dbPetData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
